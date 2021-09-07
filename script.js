1c98299d5566ecde97509a2f8a31d8f0b86349cb
7lad48 <54816005+7lad48@users.noreply.github.com>
2021-09-07 17:29:49 +0300

    worked app refactoring (black box) pre release

--- script.js
+++ script.js
@@ -13,17 +13,20 @@ $start.addEventListener('click', startGame)
 $game.addEventListener('click', handleBoxClick)
 $gameTime.addEventListener('input', setGameTime)
 
+function show($el) {
+    $el.classList.remove('hide')
+}
+function hide($el) {
+    $el.classList.add('hide')
+}
 function startGame() {
     score = 0
     setGameTime()
     // заблокировать инпут во время игры
     $gameTime.setAttribute('disabled', 'true')
-    $timeHeader.classList.remove('hide')
-    $resultHeader.classList.add('hide')
-
     isGameStarted = true
     $game.style.backgroundColor = '#fff'
-    $start.classList.add('hide')
+    hide($start)
 
     //---запуск интервала  (таймер)
     let interval = setInterval(function() {
@@ -47,17 +50,19 @@ function setGameScore() {
 function setGameTime() {
     var time = +$gameTime.value // или parseInt или + чтоб привести к числу
     $time.textContent = time.toFixed(1)
+    show($timeHeader)
+    hide($resultHeader)
 }
 function endGame() {
 
     isGameStarted = false
     setGameScore()
     $gameTime.removeAttribute('disabled')
-    $start.classList.remove('hide')
+    show($start)
     $game.innerHTML = ''
     $game.style.backgroundColor = '#ccc'
-    $timeHeader.classList.add('hide')
-    $resultHeader.classList.remove('hide')
+    hide($timeHeader)
+    show($resultHeader)
 }
 function handleBoxClick(event) {
     if(!isGameStarted) {
