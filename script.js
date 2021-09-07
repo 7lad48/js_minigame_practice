967d3a80c8e102b9e9e642e287f02e2c5d898f00
7lad48 <54816005+7lad48@users.noreply.github.com>
2021-09-07 12:33:50 +0300

    button strat, static box\

---
+++ script.js
@@ -0,0 +1,22 @@
+let $start = document.querySelector('#start')
+let $game = document.querySelector('#game')
+
+$start.addEventListener('click', startGame)
+
+function startGame() {
+    $game.style.backgroundColor = '#fff'
+    $start.classList.add('hide')
+    renderBox()
+}
+
+function renderBox() {
+    let box = document.createElement('div')
+
+    box.style.height = box.style.width = '50px'
+
+    box.style.position = 'absolute'
+    box.style.backgroundColor = '#000'
+    box.style.top = '50px'
+    box.style.left = '70px'
+    $game.insertAdjacentElement('afterbegin', box)
+}
