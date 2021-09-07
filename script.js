49495e9161915e72fdf3af66b1358377ef764f5c
7lad48 <54816005+7lad48@users.noreply.github.com>
2021-09-07 13:07:22 +0300

    refresh box when clicked

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 let $start = document.querySelector('#start')
 let $game = document.querySelector('#game')
+let score = 0
 
 $start.addEventListener('click', startGame)
-
+$game.addEventListener('click', handleBoxClick)
 function startGame() {
     $game.style.backgroundColor = '#fff'
     $start.classList.add('hide')
     renderBox()
 }
-
+function handleBoxClick(event) {
+    if(event.target.dataset.box) {
+        score++
+        renderBox()
+    }
+}
 function renderBox() {
+    $game.innerHTML = ''
     let box = document.createElement('div')
 
     box.style.height = box.style.width = '50px'
@@ -18,5 +25,7 @@ function renderBox() {
     box.style.backgroundColor = '#000'
     box.style.top = '50px'
     box.style.left = '70px'
+    box.style.cursor = 'pointer'
+    box.setAttribute('data-box', 'true') // а без true? просто при наличии атрибута??
     $game.insertAdjacentElement('afterbegin', box)
 }
